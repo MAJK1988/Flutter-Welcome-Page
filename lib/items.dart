a09e932b03abc27217ef0ecf37145c8745d944cb
Mahmoud KADDOUR <m.kaddour88@gmail.com>
2021-10-18 21:25:01 +0200

    Welcome page

---
+++ lib/items.dart
@@ -0,0 +1,17 @@
+List items = [
+  {
+    "header": "Description",
+    "description": "Real time online monitoring of power electric consumption",
+    "image": "assets/images/1.PNG"
+  },
+  {
+    "header": "Show data",
+    "description": "Power electric consumption weekly and daily report",
+    "image": "assets/images/ShowData.png"
+  },
+  {
+    "header": "Notification",
+    "description": "Send notification when malfunction appear",
+    "image": "assets/images/sendData.png"
+  }
+];
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'my_home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome Screen',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // ignore: prefer_const_constructors
+      home: MyHomePage(title: 'Welcome Screen Demo'),
+    );
+  }
+}
